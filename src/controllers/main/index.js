da4628e222a1e20a1e81f7a408563f013fa26c86
Daen Lauren Rebel <daen@index.nl>
2020-03-24 09:52:45 +0100

    WIP

--- src/controllers/main/index.js
+++ src/controllers/main/index.js
@@ -99,18 +99,97 @@ module.exports.publish = async (req, res) => {
     _attachments,
   } = body;
 
-  console.log(versions);
+  await S3.getObject({ Key: `packages/${name}/manifest.json`, Bucket: AWS_BUCKET }, (err, data) => {
+    // Handle any error and exit
+    if (err) return err;
+
+    // No error happened
+    // Convert Body from a Buffer to a String
+
+    const objectData = data.Body.toString('utf-8'); // Use the encoding necessary
+    console.log(JSON.parse(objectData));
+    return JSON.parse(objectData);
+  });
+
+  const manifestData = {
+    _id: name,
+    _rev: name,
+    name,
+    'dist-tags': {
+      latest: '0.9.0',
+    },
+    versions: {
+      '0.9.0': {
+        name: 'test',
+        version: '0.9.0',
+        description: '',
+        main: 'index.js',
+        scripts: { test: 'echo "Error: no test specified" && exit 1' },
+        author: '',
+        license: 'ISC',
+        readme: 'ERROR: No README data found!',
+        _id: 'test@0.9.0',
+        _nodeVersion: '12.16.0',
+        _npmVersion: '6.13.4',
+        dist: {
+          integrity: 'sha512-gEBrqMVQG799Va4Sk5MFC3qAhSpJojnAKa39fU521pk5Nl48X8HXRGlJXnCya3Q5WRXYAKZqcSCPJRhxs9ZDVA==',
+          shasum: '525234f31943877a0568b1df440d58139342fa9a',
+        },
+      },
+    },
+    time: {
+      created: '2019-12-02T14:03:27.976Z',
+      modified: '2019-12-02T14:04:06.415Z',
+      '0.9.0': '2020-02-02T14:03:28.324Z',
+    },
+    maintainers: [
+      {
+        name: 'daenrebel',
+        email: 'daen@index.nl',
+      },
+    ],
+    description: '',
+    homepage: '',
+    repository: {},
+    author: {
+      name: 'Daen Rebel',
+    },
+    license: 'MIT',
+    readme: 'ERROR: No README data found!',
+    readmeFilename: '',
+  };
+
+  const versionName = Object.keys(versions)[0];
+
+  const versionData = versions[versionName];
+
+  manifestData.versions[versionName] = versionData;
+  manifestData['dist-tags'].latest = versionName;
+  manifestData.time[versionName] = new Date();
 
   const packageFileName = Object.keys(_attachments)[0];
 
   const { data } = _attachments[packageFileName];
 
+  console.log(manifestData);
+
+  const packageKey = `packages/${name}/${packageFileName}`;
+  const manifestKey = `packages/${name}/manifest.json`;
+
   const packageData = Buffer.from(data, 'base64');
 
   await S3.putObject({
     Bucket: AWS_BUCKET,
-    Key: `packages/${name}/${packageFileName}`,
+    Key: packageKey,
     Body: packageData,
+  }, (response) => {
+    console.log(response);
+  });
+
+  await S3.putObject({
+    Bucket: AWS_BUCKET,
+    Key: manifestKey,
+    Body: JSON.stringify(manifestData),
   }, (response) => {
     console.log(response);
   });
